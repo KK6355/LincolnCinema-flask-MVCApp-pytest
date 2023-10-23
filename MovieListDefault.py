34a27d0dbce21ee403d2cba78518c9a855e377ce
vicky kang <vicky.kang@payitnow.info>
2023-10-23 17:49:56 +1300

    added movie API request data

---
+++ MovieListDefault.py
@@ -0,0 +1,9 @@
+from models.Movie import Movie
+
+movie1 = Movie("Killers of the Flower Moon","English","Crime","20 Oct 2023")
+movie2 = Movie("Taylor Swift: The Eras Tour","English","Music","12 Oct 2023")
+movie3 = Movie("Dumb Money","English","Comedy","29 Sep 2023")
+movieListDefault = [movie1,movie2,movie3]
+# for movie in movieListDefault:
+#     print(movie.title)
+# print(len(movieListDefault))
---
+++ models/Movie.py
@@ -0,0 +1,49 @@
+import requests
+class Movie:
+    """! The Movie class
+    Defines class for movies
+    """
+    nextId = 4000
+    APIKey = '1015babb'
+    
+    def __init__(self,title,language,genre,reDate):
+        self.__movieId = Movie.nextId
+        self.__title = title
+        self.__language = language
+        self.__genre = genre
+        self.__reDate = reDate
+        Movie.nextId += 1
+        self.movieList = []
+        self.data = requests.get(f'http://www.omdbapi.com/?t={self.title}&apikey={self.APIKey}').json()
+        
+    @property
+    def movieId(self):
+        return self.__movieId
+    @property
+    def title(self):
+        return self.__title
+    @property
+    def language(self):
+        return self.__language
+    @property
+    def genre(self):
+        return self.__genre
+    @property
+    def reDate(self):
+        return self.__reDate
+    @property
+    def poster(self): 
+        return self.data["Poster"]      
+    @property
+    def country(self): 
+        return self.data["Country"]
+    @property
+    def runtime(self): 
+        return self.data["Runtime"]
+    @property
+    def description(self): 
+        return self.data["Plot"]    
+
+    
+    def info(self):
+        print(f"Movie Id:{self.movieId},title:{self.title}, language:{self.language}, genre:{self.genre}, released date:{self.reDate}")
