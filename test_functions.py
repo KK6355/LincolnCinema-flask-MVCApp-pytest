57aa8e153b51818852191efa29277814baf6fcdd
vicky kang <vicky.kang@payitnow.info>
2023-11-01 13:20:12 +1300

    added tesing cases

--- test_functions.py
+++ test_functions.py
@@ -1,6 +1,10 @@
 from app import app
 import flask
 import os
+from models.Customer import Customer
+from CustomerListData import customerList
+from MovieListDefault import movieListDefault
+from ScreenListData import screenList
 os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
 def test_index():  
     with app.test_client() as test_client:
@@ -20,6 +24,12 @@ def test_tickets():
         if "role" in flask.session and flask.session['role']=='staff':
             assert response.status_code == 200
             assert b"Tickets" in response.data
+def test_register():
+    with app.test_client() as test_client:
+        response = test_client.post('/register',data={"userName":"test@test.com","password":"123456"})
+        if response.status_code == 200:
+            assert len(customerList) == 3
+            assert customerList[len(customerList)-1].userName == "test@test.com"
 def test_login():
     with app.test_client() as test_client:
         response = test_client.post('/login')
@@ -49,9 +59,17 @@ def test_manageMovies():
             assert b"Manage Movies & Screens" in response.data
 def test_addMovie():
     with app.test_client() as test_client:
-        response = test_client.post('/addMovie')
+        response = test_client.post('/addMovie',data={"title":"Harry Potter","language":"English","genre":"Adventure","reDate":"2000-1-1"})
         if response.status_code == 200:
             assert b'Manage Movies & Screens' in response.data
-
+            assert len(movieListDefault) == 5
+            assert movieListDefault[len(movieListDefault)-1].title == "Harry Potter"
+def test_addScreen():
+    with app.test_client() as test_client:
+        response = test_client.post('/addScreen',data={"movieId":4000, "hallId":1, "scheduledDate":"2023-11-30", "scheduledTime":"10:00"})
+        if response.status_code == 200:
+            assert b'Manage Movies & Screens' in response.data
+            assert len(screenList) == 10
+            assert screenList[len(screenList)-1].movieId == 4000
 
                 
